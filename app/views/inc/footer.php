b53d596bd0a2703c20f41709fdccb9a40d72673f
jlm222 <59456904+jlm222@users.noreply.github.com>
2021-05-03 17:31:30 +0100

    Adjusts footer link spacing

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -1,8 +1,8 @@
     <footer class="footer">
         <div class="social">
             <ul class="social__list">
-                <li class="social__item"><a href="https://www.instagram.com/urukhardplayhard" class="social__link">Instagram &nbsp;<i class="fab fa-instagram"></i></a></li>
-                <li class="social__item"><a href="https://www.patreon.com/urukhardplayhard" class="social__link">Patreon &nbsp;<i class="fab fa-patreon"></i></a></li>
+                <li class="social__item"><a href="https://www.instagram.com/urukhardplayhard" class="social__link social__link--instagram">Instagram &nbsp;<i class="fab fa-instagram"></i></a></li>
+                <li class="social__item"><a href="https://www.patreon.com/urukhardplayhard" class="social__link social__link--patreon">Patreon &nbsp;<i class="fab fa-patreon"></i></a></li>
             </ul>
         </div>
     
